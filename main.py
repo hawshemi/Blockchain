e7898abd3f5dd7ba9ab686004aadcbd4ff5e4357
R Hashemi <rhawshemi@gmail.com>
2022-12-06 16:29:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -236,4 +236,4 @@ for code in default_exceptions:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
